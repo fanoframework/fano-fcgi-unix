c6b12a836e3078a0b23812cf08164fe72a2f62ea
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-07-20 18:53:20 +0800

    update fano repo

--- src/app.pas
+++ src/app.pas
@@ -16,13 +16,13 @@ var
     appInstance : IWebApplication;
 
 begin
-    writeln('Starting application at /tmp/fano-fcgi/fano-fcgi-unix.sock');
+    writeln('Starting application at /tmp/fano-fcgi-unix.sock');
 
     (*!-----------------------------------------------
      * Bootstrap FastCGI application
      *
      * @author AUTHOR_NAME <author@email.tld>
      *------------------------------------------------*)
-    appInstance := TBootstrapApp.create('/tmp/fano-fcgi/fano-fcgi-unix.sock');
+    appInstance := TBootstrapApp.create('/tmp/fano-fcgi-unix.sock');
     appInstance.run();
 end.
